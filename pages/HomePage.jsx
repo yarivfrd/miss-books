f02171b0b2b2f8f435aadafdc7a6e787c1442088
Yariv Fruend <yarivfrd@gmail.com>
2025-06-01 14:35:17 +0300

    updated homepage and about page

---
+++ pages/HomePage.jsx
@@ -0,0 +1,12 @@
+
+export function HomePage() {
+
+    return (
+        <section className="home">
+            <span className="page-title">
+                Welcome to <h1>Miss Books</h1>
+            </span>
+            <h2>Good books for a good price.</h2>
+        </section>
+    )
+} 
