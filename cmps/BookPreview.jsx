7a275f48d23b4f3e252b200f5dbd76047e40e1f8
Yariv Fruend <yarivfrd@gmail.com>
2025-06-03 09:28:23 +0300

    Book preview cmp - added an on sale sign for books on sale

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -4,7 +4,9 @@ export function BookPreview({ book }) {
     return (
         <article className="book-preview">
             <h2>{book.title}</h2>
-            <img src={book.thumbnail} alt="book-cover" />
+            <div className={`image-container ${book.listPrice.isOnSale ? 'on-sale' : ''}`}>
+                <img src={book.thumbnail} alt="book-cover" />
+            </div>
         </article>
     )
 }
