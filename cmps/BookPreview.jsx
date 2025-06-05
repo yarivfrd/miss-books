879e14c590e570483a96ae51f1f7ef2a12a0219f
Yariv Fruend <yarivfrd@gmail.com>
2025-06-05 15:27:41 +0300

    BookPreview cmp styling

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -3,10 +3,10 @@ export function BookPreview({ book }) {
 
     return (
         <article className="book-preview">
-            <h2>{book.title}</h2>
             <div className={`image-container ${book.listPrice.isOnSale ? 'on-sale' : ''}`}>
                 <img src={book.thumbnail} alt="book-cover" />
-            </div>
+                </div>
+            <h2>{book.title}</h2>
         </article>
     )
 }
