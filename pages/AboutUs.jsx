4e6cb291c7976ec6c02a8d48417547b34f27bfb9
Yariv Fruend <yarivfrd@gmail.com>
2025-06-05 15:27:57 +0300

    AboutUs cmp styling

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -7,8 +7,9 @@ export function AboutUs() {
         <div className="about">
             <h2>About books and us...</h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio dolore sapiente, iste animi corporis nisi atque tempora assumenda dolores. Nobis nam dolorem rerum illo facilis nemo sit voluptatibus laboriosam necessitatibus!</p>
+            <hr />
             <LongTxt />
-        </section>
+        </div>
 
     )
 }
