fe9c9db177ba8ccd159c9b9a432af0a00216d84b
Yariv Fruend <yarivfrd@gmail.com>
2025-06-05 08:54:17 +0300

    HomePage cmp styling

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -2,11 +2,12 @@
 export function HomePage() {
 
     return (
-        <section className="home">
-            <span className="page-title">
-                Welcome to <h1>Miss Books</h1>
-            </span>
-            <h2>Good books for a good price.</h2>
-        </section>
+        <main className="home">
+            <section className="welcome">
+                <img src="../assets/img/logo.png" alt="Miss Books logo" />
+                <h2 className="page-title">Welcome to Miss Books!</h2>
+                <h3>Good books for a good price</h3>
+            </section>
+        </main>
     )
 } 
