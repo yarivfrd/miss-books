1ae4337f0bbc5d78de2dd3d83c61d7a994e0ac7b
Yariv Fruend <yarivfrd@gmail.com>
2025-06-05 16:52:38 +0300

    Fixed image path

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -4,7 +4,7 @@ export function HomePage() {
     return (
         <div className="home">
             <section className="welcome">
-                <img src="../assets/img/logo.png" alt="Miss Books logo" />
+                <img src="https://raw.githubusercontent.com/yarivfrd/miss-books/refs/heads/main/assets/img/logo.png" alt="Miss Books logo" />
                 <h2 className="page-title">Welcome to Miss Books!</h2>
                 <h3>Good books for a good price</h3>
             </section>
