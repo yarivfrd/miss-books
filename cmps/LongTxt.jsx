a2137a02e3dfbb301ea6baf00c953be2a25141f7
Yariv Fruend <yarivfrd@gmail.com>
2025-06-12 08:51:40 +0300

    LongTxt cmp refactoring

--- cmps/LongTxt.jsx
+++ cmps/LongTxt.jsx
@@ -1,21 +1,17 @@
-
+import { makeLorem } from "../services/util.service.js";
 const { useState } = React;
 
-export default function LongTxt({ txt, length = 100 }) {
+const dummyText = makeLorem()
+
+export default function LongTxt({ txt = dummyText, length = 100 }) {
 
     const [isCollapsed, setIsCollapsed] = useState(false);
-
-    txt = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-
-    function handleCollpaseToggle() {
-        setIsCollapsed(curr => !curr);
-    }
 
     return (
         <div className="LongTxt">
             <p>
                 {isCollapsed ? txt : `${txt.substring(0, length).trim()}... `}
-                <a onClick={handleCollpaseToggle}> Show {isCollapsed ? 'less' : 'more'}</a>
+                <a onClick={() => setIsCollapsed(curr => !curr)}> Show {isCollapsed ? 'less' : 'more'}</a>
             </p>
             
         </div>
