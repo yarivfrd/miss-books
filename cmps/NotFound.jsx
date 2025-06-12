f83d5b1b223b58f832e21b5e29a78f4adfb927a2
Yariv Fruend <yarivfrd@gmail.com>
2025-06-12 13:37:40 +0300

    re-styled notFound cmp

--- cmps/NotFound.jsx
+++ cmps/NotFound.jsx
@@ -1,16 +1,16 @@
-const { Link } = ReactRouterDOM
+const { useNavigate } = ReactRouterDOM;
 
 export function NotFound() {
 
+    const navigate = useNavigate();
+
     return (
-        <section className="not-found-container">
-            <section className="not-found">
-                <h2>Ooops... Error <span style={{ color: 'red' }}>404</span></h2>
-            </section>
+        <section className="not-found">
+            <h2>Ooops... Error <span style={{ color: 'red' }}>404</span></h2>
             <p>
                 Sorry but the page you are looking for does not exist.
             </p>
-            <Link to="/">Back</Link>
+            <button onClick={() => {navigate('/')}}>Back Home</button>
         </section>
     )
 }
