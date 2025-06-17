297d3c85059bc398fe2add56a383abafa1d08296
Yariv Fruend <yarivfrd@gmail.com>
2025-06-17 20:18:39 +0300

    Reset button is design now refelects more clearly its drastic action

--- cmps/BookAside.jsx
+++ cmps/BookAside.jsx
@@ -7,15 +7,15 @@ export function BookAside({ onSetFilter, filterBy, onBookReset }) {
     const navigate = useNavigate();
 
     return (
-        <aside>
-            <button onClick={() => navigate('/book/edit')}>Add New Manually</button>
-            <button onClick={() => navigate('/book/add')}>Add New From Google</button>
+        <aside className="book-aside">
+            <button onClick={() => navigate('/book/edit')}>Add</button>
+            <button onClick={() => navigate('/book/add')}>Add Google Book</button>
             <button onClick={() => navigate('/book/stats')}>Stats</button>
             <BookFilter
                 onSetFilter={onSetFilter}
                 filterBy={filterBy}
             />
-            <button onClick={onBookReset}>Reset Books</button>
+            <button className="reset-books-btn" onClick={onBookReset}>Reset Books</button>
         </aside>
     )
 }
