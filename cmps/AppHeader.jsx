e759cb32cce8f21b09d8261332a5ef7f14e2b842
Yariv Fruend <yarivfrd@gmail.com>
2025-07-04 16:25:38 +0300

    Fixed logo file path

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -7,7 +7,7 @@ export function AppHeader() {
     return (
         <header className="app-header full main-layout animate__animated animate__fadeInDown">
             <section>
-                <img onClick={() => {navigate('/')}} className="logo" src="../assets/img/logo.svg" alt="logo" />
+                <img onClick={() => { navigate('/') }} className="logo" src="https://raw.githubusercontent.com/yarivfrd/miss-books/ffb7fb1b88215a362b54abf56fb7dfd7c7526203/assets/img/logo.svg" alt="logo" />
                 <nav className="app-nav">
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/about-us">About</NavLink>
